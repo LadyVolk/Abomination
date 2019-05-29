bb6a500f18d984a7d40f0d7c7f137a8d71a37fe6
LadyVolk <LadyVolk@users.noreply.github.com>
2019-05-29 17:43:59 +0100

    fixed blocks

--- levels/1.lua
+++ levels/1.lua
@@ -2,20 +2,20 @@ local block_create = require "block"
 local level = {
   blocks = {
     --blocks above
-    block_create({400, 390}, 30, false),
-    block_create({430, 390}, 30, false),
-    block_create({460, 390}, 30, false),
+    block_create({400, 400}, 30, false),
+    block_create({430, 400}, 30, false),
+    block_create({460, 400}, 30, false),
 
     --right of fall block
-    block_create({440, 430}, 30, false),
-    block_create({470, 430}, 30, false),
+    block_create({430, 430}, 30, false),
+    block_create({460, 430}, 30, false),
 
     block_create({400, 430}, 30, true),
 
     --blocks below
-    block_create({430, 470}, 30, false),
-    block_create({460, 470}, 30, false),
-    block_create({400, 470}, 30, false),
+    block_create({430, 460}, 30, false),
+    block_create({460, 460}, 30, false),
+    block_create({400, 460}, 30, false),
   },
 
   player_ipos = {200, 100},
