76211123c067552c51826ad5bf8e281f1a9207f8
LadyVolk <LadyVolk@users.noreply.github.com>
2019-06-02 11:49:59 +0100

    Created first test puzzle

--- levels/1.lua
+++ levels/1.lua
@@ -6,10 +6,18 @@ local level = {
     block_create({430, 400}, 30, false, false),
     block_create({460, 400}, 30, false, false),
 
+    --left of fall blocks
+    block_create({340, 400}, 30, false, false),
+    block_create({340, 430}, 30, false, false),
+    block_create({340, 460}, 30, false, false),
+    block_create({370, 460}, 30, false, false),
+
     --right of fall block
     block_create({430, 430}, 30, false, true),
+
     block_create({460, 430}, 30, false, false),
 
+    --fall block
     block_create({400, 430}, 30, true, false),
 
     --blocks below
