acb1e83f04906441f7004b88043976c46818259a
LadyVolk <LadyVolk@users.noreply.github.com>
2019-07-25 17:45:26 +0100

    created area for resizing blocks

--- gamestates/level-editor.lua
+++ gamestates/level-editor.lua
@@ -23,15 +23,51 @@ function _state:keypressed(key)
 end
 
 function _state:mousemoved(x, y, dx, dy)
+  CURSOR.setcursor("normal")
   if _selected_block and _is_dragging then
     _selected_block.pos[1] = x
     _selected_block.pos[2] = y
+  end
+  if _selected_block then
+    local scale = 0.5
+    local fixed_thickness = 10
+    local upper_block = {
+        pos = {_selected_block.pos[1],
+               _selected_block.pos[2]-_selected_block.height/2},
+        width = _selected_block.width*scale,
+        height = fixed_thickness,
+      }
+      local lower_block = {
+        pos = {_selected_block.pos[1],
+               _selected_block.pos[2]+_selected_block.height/2},
+        width = _selected_block.width*scale,
+        height = fixed_thickness,
+      }
+      local right_block = {
+        pos = {_selected_block.pos[1]-_selected_block.width/2,
+               _selected_block.pos[2]},
+        width = fixed_thickness,
+        height = _selected_block.height*scale,
+      }
+      local left_block = {
+        pos = {_selected_block.pos[1]+_selected_block.width/2,
+               _selected_block.pos[2]},
+        width = fixed_thickness,
+        height = _selected_block.height*scale,
+
+      }
+    if Physics.collision_point_rect({x = x, y = y}, upper_block) or
+       Physics.collision_point_rect({x = x, y = y}, lower_block) or
+       Physics.collision_point_rect({x = x, y = y}, right_block) or
+       Physics.collision_point_rect({x = x, y = y}, left_block) then
+      CURSOR.setcursor("resize")
+    end
   end
 end
 
 function _state:mousepressed(x, y, button, isTouch)
   if button == 3 then
-    table.insert(_elements, Block({size = {30, 30}, pos = {x, y}}))
+    table.insert(_elements, Block({size = {100, 100}, pos = {x, y}}))
   elseif button == 2 then
     local block, i = _find_block(x, y)
     if block then
